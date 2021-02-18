a7b7f6376dbd0d5d96367d530bf7e686a16b0205
Flo-Nz <63399869+Flo-Nz@users.noreply.github.com>
2021-02-18 19:17:15 +0100

    commands are now dynamically retrieved

--- index.js
+++ index.js
@@ -1,38 +1,82 @@
-const Discord = require('discord.js');
-const dotenv = require('dotenv');
-dotenv.config();
+/**************************************************
+ * 
+ * REQUIRES SECTION ********
+ * 
+ * ***********************************************/
 
-const client = new Discord.Client();
+const fs = require('fs'); // file reader
+const Discord = require('discord.js'); // discord.js lib
+const dotenv = require('dotenv'); // to access .env
+dotenv.config(); // to launch process.env
+const prefix = process.env.PREFIX;
 
-client.login();
+/**************************************************
+ * 
+ * COMMANDS COLLECTION PREPARATION SECTION ********
+ * 
+ * ***********************************************/
+const client = new Discord.Client(); // start a new client
+
+// Our commands will be available in a Discord Collection (which is an extended class of a JS Map)
+client.commands = new Discord.Collection();
+// dynamically retrieve all created command files.
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// The fs.readdirSync() method will return an array of all the file names in that directory, e.g. ['ping.js', 'beep.js']. 
+// The filter is there to make sure any non-JS files are left out of the array. With that array, you can loop over it and dynamically set your commands to the Collection you made above.
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+
+/**************************************************
+ * 
+ ***** DYNAMIC EXECUTION OF COMMANDS SECTION*******
+ * 
+ * ***********************************************/
+
+client.on("message", function (message) {
+
+    // We don't want our bot to answer other bot's messages 
+    if (message.author.bot || !message.content.startsWith(prefix)) {
+        return;
+    }
+    
+
+    // Define a commandbody which will contain the content of the message after the prefix (thanks to slice method)
+    const commandBody = message.content.slice(prefix.length);
+    // Args is an array containing the command and the arguments (the split method uses a regex in order to ignore multiple spaces in the commandbody)
+    const args = commandBody.trim().split(/ +/);
+    // Shift method will detete the first arg inside args and return it : the first arg should be our command 
+    const command = args.shift().toLowerCase();
+
+    // If our commands collection doesn't have our command, stop there.
+    if (!client.commands.has(command)) {
+        message.reply('cette commande n\'existe pas !')
+        return;
+    }
+
+    // If a command is find, try to execute it. If it doesn't work, just send an error message but this will not crash our bot.
+    try {
+	    client.commands.get(command).execute(message, args);
+    } catch (error) {
+	    console.error(error);
+	    message.reply('Il y a eu une error dans l\'exécution de cette commande !');
+    }
+});
+
+/**************************************************
+ * 
+ ************* STARTING SECTION *******************
+ * 
+ * ***********************************************/
+
 client.on('ready', () => {
     console.log(`${client.user.username} is ready !`);
 });
 
-// On configure le préfixe de commande 
-const prefix = process.env.PREFIX;
-
-// Ce morceau de code permet de récupérer chaque message posté sur un serveur.
-client.on("message", function (message) {
-
-    // On ne veut pas que le bot réponde aux messages d'autres bots, on lui demande donc d'ignorer les messages dont l'auteur est un bot.
-    if (message.author.bot) {
-        return;
-    }
-    // On ne veut pas que le bot réponde aux messages qui ne commencent pas par le prefix 
-    if (!message.content.startsWith(prefix)) {
-        return;
-    }
-
-    // On initialise un commandBody qui contiendra le contenu entier de la commande tapée par l'utilisateur, en décalant la prise en compte de la longueur du préfixe
-    const commandBody = message.content.slice(prefix.length);
-    // On récupère un tableau contenant le nom de la commande (après le  !) ainsi que les éventuels arguments (après le !cmd )
-    const arguments = commandBody.trim().split(/ +/);
-    // On supprime le premier argument du tableau (il s'agit de la commande) et on la renvoie, après conversion en minuscule, à la constante command
-    const command = arguments.shift().toLowerCase();
-
-    if (command === "ping") {
-        message.reply('Pong !');
-    }
-
-});
+client.login();
